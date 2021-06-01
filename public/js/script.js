5984867567c63c2b2b86b4fac3070ab6a3d6fd6e
Fandi Arfa <arfabuma.987@gmail.com>
2021-06-01 12:17:16 +0700

    new version

---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+var myFullpage = new fullpage('#fullpage', {
+  navigation:true,
+  navigationPosition: 'right',
+  anchors:['intro', 'education', 'skill', 'favorites']
+});
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+  });
+
+  (function() {
+    // Add event listener
+    document.addEventListener("mousemove", parallax);
+    const elem = document.querySelector(".bg-parallax");
+    // Magic happens here
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let x = `${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
+    }
+
+})();
+
+function hover(element) {
+  element.setAttribute('src', 'images/photo.jpg');
+}
+
+function unhover(element) {
+  element.setAttribute('src', 'images/illust.png');
+}
